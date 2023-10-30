aac5244acaa053c3b0d15f016ebe4adbd475d5f5
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2023-10-30 22:54:25 +0100

    add ability to parse header chunk
    also add scaffolding for main datastream parsing (yet to test anything)

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Parser where
 
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
+import Data.Functor
 import Data.Int (Int32, Int8)
 import Data.Void
 import Data.Word
 import Text.Megaparsec
 import Text.Megaparsec.Byte
+import Text.Megaparsec.Byte.Binary
 
 type Parser = Parsec Void B.ByteString
 type BitDepth = Int8
@@ -16,16 +19,37 @@ type BitDepth = Int8
 data ColourType = Greyscale Bool | Truecolour Bool | IndexedColour
     deriving (Show)
 data ImageType = ImageType {colourType :: ColourType, bitDepth :: BitDepth}
-data ImageHeader = ImageHeader {width :: Int32, height :: Int32, imageType :: ImageType, interlace :: Bool}
+data ImageHeader = ImageHeader {width :: Word32, height :: Word32, imageType :: ImageType, interlace :: Bool}
 data Chunk = IHDR ImageHeader | PLTE
 
--- nthBit :: Int -> Word8 -> Bool
--- nthBit n word = (word .>>. n) .&. 1 == 1
+pPNGBytestream :: Parser [Chunk]
+pPNGBytestream = string (B.pack [137, 80, 78, 71, 13, 10, 26, 10]) *> many pChunk
+
+pChunk :: Parser Chunk
+pChunk = do
+    length <- word32be
+    choice
+        [ string "IHDR" *> (IHDR <$> pImageHeaderData)
+        , string "PLTE" $> PLTE -- TODO: fix
+        ]
+        <* count 4 anySingle -- cyclic redundancy check, assuming data isn't corrupt for now
+
+pImageHeaderData :: Parser ImageHeader
+pImageHeaderData =
+    ImageHeader
+        <$> word32be
+        <*> word32be
+        <*> parseImageType
+        <* count 2 (char 0) -- compression and filter method
+        <*> pBool
+
+pBool :: Parser Bool
+pBool = (== 1) <$> (char 0 <|> char 1)
 
 parseImageType :: Parser ImageType
 parseImageType = do
-    bitDepth <- toEnum . fromEnum <$> anySingle
-    colourType <- oneOf [0, 2, 3, 4, 6] >>= byteToColourType
+    bitDepth <- parseDepth
+    colourType <- parseColourType
     if allowedImageType colourType bitDepth
         then pure ImageType{..}
         else
@@ -36,25 +60,21 @@ parseImageType = do
                 ++ show bitDepth
                 ++ ")"
 
+parseDepth :: Parser BitDepth
+parseDepth = toEnum . fromEnum <$> oneOf [1, 2, 4, 8, 16]
+
+-- TODO: get better error message by making this function take depth as input
+parseColourType :: Parser ColourType
+parseColourType =
+    choice
+        [ char 0 $> Greyscale False
+        , char 2 $> Truecolour False
+        , char 3 $> IndexedColour
+        , char 4 $> Greyscale True
+        , char 6 $> Truecolour True
+        ]
+
 allowedImageType :: ColourType -> BitDepth -> Bool
 allowedImageType (Greyscale False) = flip elem [1, 2, 4, 8, 16]
 allowedImageType IndexedColour = flip elem [1, 2, 4, 8]
 allowedImageType _ = flip elem [8, 16]
-
-byteToColourType :: Word8 -> Parser ColourType
-byteToColourType 0 = pure $ Greyscale False
-byteToColourType 2 = pure $ Truecolour False
-byteToColourType 3 = pure IndexedColour
-byteToColourType 4 = pure $ Greyscale True
-byteToColourType 6 = pure $ Truecolour True
-byteToColourType b = fail $ "expected one of [0, 2, 3, 4, 6], got " ++ show b
-
--- byteToColourType c
---     | not (b0 || b1 || b2) = Just $ Greyscale False
---     | not b0 && b1 && not b2 = Just $ Truecolour False
---     | not b0 && b1 && b2 = Just IndexedColour
---     | b0 && not b1 && not b2 = Just $ Greyscale True
---     | b0 && b1 && not b2 = Just $ Truecolour True
---     | otherwise = Nothing
---   where
---     (b0, b1, b2) = (nthBit 0 c, nthBit 1 c, nthBit 2 c)
